c2a0ce490bcd9939e039e5f59ca70ff80254ab31
Karla Sahin <karlasahin@Karlas-MacBook-Pro.local>
2021-11-19 16:15:23 -0500

    open cv code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opencvexample.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opencvexample.java
@@ -1,5 +1,5 @@
 
-package org.firstinspires.ftc;
+package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
