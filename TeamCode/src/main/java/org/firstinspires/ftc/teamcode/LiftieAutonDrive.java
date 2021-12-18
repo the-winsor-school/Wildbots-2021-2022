09c4cc1fd8f022cd36df70a2225c74189e9311e7
Ella Pascucci <ellapascucci@Ellas-MacBook-Air-2.local>
2021-12-17 19:24:14 -0500

    incompleteparkinwh

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LiftieAutonDrive.java
@@ -0,0 +1,29 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.libraries.DrivingLibrary;
+
+@Autonomous(name = "auton")
+public class Liza extends LinearOpMode {
+
+    private DrivingLibrary drivingLibrary;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        drivingLibrary = new DrivingLibrary(this);
+        drivingLibrary.setSpeed(1.0);
+        telemetry.addData("status", "initialized");
+        telemetry.update();
+
+        waitForStart();
+
+        if (opModeIsActive()) {
+            drivingLibrary.bevelDrive(0, -1, 0);
+            sleep(1000);
+
+        }
+    }
+}
+
