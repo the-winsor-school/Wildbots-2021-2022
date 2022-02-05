296f117613e1494316e136a8555fa9c9c7aa228d
kenna26 <90514808+kenna26@users.noreply.github.com>
2022-02-05 13:59:33 -0500

    fixed soemthing small

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TankTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TankTeleOp.java
@@ -119,8 +119,5 @@ public class TankTeleOp extends LinearOpMode {
     public void rotiniBrake () { //turn the motor off
         tankDrive.rotini.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
     }
-
-    public void boxServos(int position) {
-        leftServo.setPosition(position);
-    }
+    
 }
