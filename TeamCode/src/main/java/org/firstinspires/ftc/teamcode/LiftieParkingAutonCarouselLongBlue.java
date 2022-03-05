86dd49860696bf80f2df4f9e192aa26f99f45cbf
lkuntz223 <72308931+lkuntz223@users.noreply.github.com>
2022-03-04 21:06:38 -0500

    Changed Blue Auton to account for the squishy wheel being on the other side. still need to fix and tweak this as well as do this to all other blue carousel autons, we also still need a blue warehouse park code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LiftieParkingAutonCarouselLongBlue.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LiftieParkingAutonCarouselLongBlue.java
@@ -25,22 +25,37 @@ public class LiftieParkingAutonCarouselLongBlue extends LinearOpMode{
         waitForStart();
 
         if (opModeIsActive()) {
-            drivingLibrary.bevelDrive(0.6f, 0, 0);
+
+            // strafe right
+            drivingLibrary.bevelDrive(1, 0, 0); // right
             sleep(400);
-
-            drivingLibrary.bevelDrive(0, 1, 0);
+            drivingLibrary.bevelDrive(0, 0, 0);
+            //go back
+            drivingLibrary.bevelDrive(0, 1, 0); //backwards
             sleep(1500);
+            drivingLibrary.bevelDrive(0, 0, 0);
+            // turn 45 degrees
+            drivingLibrary.spinToAngle(-Math.PI/4);
 
-            carousel.setPower(1);
+            // move forward
+            drivingLibrary.bevelDrive(0, 1, 0); // this ACTUALLY goes backwards(the right direction) i promise
+            sleep(300);
+            drivingLibrary.bevelDrive(0, 0, 0); // stop
+
+            // carousel
+
+            carousel.setPower(-1);
             sleep(3500);
             carousel.setPower(0);
 
-            drivingLibrary.bevelDrive(1, 0, 0);
-            sleep(350);
-
-            drivingLibrary.bevelDrive(0, -1, 0);
+            // 0 degrees
+            drivingLibrary.spinToAngle(.1f);
+            // move right a little
+            drivingLibrary.bevelDrive(1, 0, 0); // right
+            sleep(200);
+            // drive forward
+            drivingLibrary.bevelDrive(0, -1, 0); //forwards
             sleep(5000);
-
             drivingLibrary.brakeStop();
         }
     }
